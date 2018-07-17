e9ef682a63a25b8699410197b01974c67845100f
Daniel Stewart <mgdog1@googlemail.com>
2018-07-17 14:25:20 +0100

    dropdown list populating

--- src/components/PokemonSelector.jsx
+++ src/components/PokemonSelector.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 class PokemonSelector extends React.Component{
 
-render(){
-  return(<p>Selector</p>)
-
-}
-
+  render(){
+    const pokemon = this.props.pokemon.map((pokemon, index) => {
+      return <option value={index} key={index}>{pokemon.name}</option>
+    });
+    return(<select id="pokemon-selector"  defaultValue="default">
+      <option disabled value="default">Choose a pokemon...</option>
+      {pokemon}
+    </select>)
+  }
+  
 }
 export default PokemonSelector;
--- src/containers/PokemonContainer.jsx
+++ src/containers/PokemonContainer.jsx
@@ -3,10 +3,29 @@ import PokemonDetails from '../components/PokemonDetails.jsx';
 import PokemonSelector from '../components/PokemonSelector.jsx';
 
 class PokemonContainer extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      pokemon: [
+        {url: "https://pokeapi.co/api/v2/pokemon/1/",
+        name: "bulbasaur"}
+      ]
+    }
+  }
+
+  componentDidMount(){
+   const url = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+   fetch(url)
+     .then(response => response.json())
+     .then( pokemon => this.setState({pokemon: pokemon.results}))
+     .catch(err => console.error(err))
+
+  }
+
   render(){
     return(<div>
+      <PokemonSelector pokemon={this.state.pokemon}/>
       <PokemonDetails />
-      <PokemonSelector />
     </div>)
 
   }
